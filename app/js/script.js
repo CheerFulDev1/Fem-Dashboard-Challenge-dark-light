784e33ca40872370efa27622f2863d285950820d
CheerFulDev <35979863+CheerFulDev1@users.noreply.github.com>
2024-08-18 16:25:20 +0100

    added light and dark mode with a switch

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,10 @@
-console.log("hello!");
+console.log("hello!");//# sourceMappingURL=script.js.map
+
+const radioButton = document.querySelectorAll('.toggle__wrapper input');
+for (let i = 0; i < radioButton.length; i++){
+    radioButton[i].addEventListener('click', (event) => {
+        document.getElementById('dark').checked ?
+        (document.querySelector('body').classList = 'dark')
+        : (document.querySelector('body').classList = 'light');
+    });
+}
--- dist/script.js
+++ dist/script.js
@@ -1,3 +1,9 @@
 "use strict";
 
-console.log("hello!");//# sourceMappingURL=script.js.map
+console.log("hello!");
+var radioButton = document.querySelectorAll('.toggle__wrapper input');
+for (var i = 0; i < radioButton.length; i++) {
+  radioButton[i].addEventListener('click', function (event) {
+    document.getElementById('dark').checked ? document.querySelector('body').classList = 'dark' : document.querySelector('body').classList = 'light';
+  });
+}//# sourceMappingURL=script.js.map
